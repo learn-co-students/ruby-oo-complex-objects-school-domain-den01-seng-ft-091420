1de4fa18fbec6b09a9e95c5bfde96b9a3b3d5b84
jason1110 <Jason.easterly11@gmail.com>
2020-09-18 15:57:09 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School    
+attr_reader :name
+attr_accessor :roster
+
+    def initialize name
+            @name = name
+            @roster = {}
+
+    end
+
+    def add_student (student_name, grade) 
+        grade_roster = @roster[grade] ||= []
+        grade_roster << student_name        
+
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end 
+
+    def sort
+        @roster.each do |_, names|
+            names.sort!
+        end     
+    end    
+    
+end
+
+
+#  school = School.new("tvhs")
+
+#  p school.add_student("kelly", 9)
